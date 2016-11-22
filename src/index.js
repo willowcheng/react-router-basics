a7ab7b3eb635fa29128073753fff54075e898c9f
willowcheng <e.wingus@gmail.com>
2016-11-22 15:58:37 -0500

    Install React router and declare a route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 // Libs
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
+import {Router, Route, hashHistory } from 'react-router';
 
 // CSS 
 import './css/style.css';
 
 // Components
 import App from './components/App';
+import Home from './components/Home';
 
 // Render
-render(
-  <App />,
-  document.getElementById('root')
+render((
+    <Router history={hashHistory}>
+        <Route path="/" component={Home} />
+    </Router>
+    ), document.getElementById('root')
 );
