e4be99a75a379a4dcc46ff21a7bcc50069be38b6
willowcheng <e.wingus@gmail.com>
2016-11-22 16:06:15 -0500

    Declare multiple routes

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ import './css/style.css';
 // Components
 import App from './components/App';
 import Home from './components/Home';
+import About from './components/About';
+import Courses from './components/Courses';
+import Teachers from './components/Teachers';
 
 // Render
 render((
     <Router history={hashHistory}>
         <Route path="/" component={Home} />
+        <Route path="about" component={About} />
+        <Route path="teachers" component={Teachers} />
+        <Route path="courses" component={Courses}/>
     </Router>
     ), document.getElementById('root')
 );
