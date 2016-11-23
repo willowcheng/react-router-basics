87d4d196da88782c771afae5fe64399c1ec8faf8
willowcheng <e.wingus@gmail.com>
2016-11-23 14:55:51 -0500

    Routes with parameters

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const Featured = () => {  
-  return (
-    <div className="main-content">
-      <h2>Featured: </h2>
-      <p>Introducing <strong> </strong>, a teacher who loves teaching courses about <strong> </strong>!</p>
-    </div>
-  );
-}
+const Featured = props => {
+    let topic = props.params.topic;
+    let name = props.params.name;
+
+    return (
+        <div className="main-content">
+            <h2>{name}</h2>
+            <p>Introducing <strong>{name}</strong>, a teacher who loves teaching courses about <strong>{topic}</strong>!</p>
+        </div>
+    );
+};
 
 export default Featured;
