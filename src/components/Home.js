28eef98463d28032cc1990c2f0fd8225ce7c5f90
willowcheng <e.wingus@gmail.com>
2016-11-23 15:02:09 -0500

    Navigate routes programmatically

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
 import React, {Component} from 'react';
+import { browserHistory } from 'react-router';
 import {Link} from 'react-router';
 
 class Home extends Component {
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let teacherName = event.target.elements[0].value;
+        let teacherTopic = event.target.elements[1].value;
+        let path = `featured/${teacherTopic}/${teacherName}`;
+        browserHistory.push(path);
+    }
+
     render() {
         return (
             <div className="main-content home">
@@ -15,7 +25,11 @@ class Home extends Component {
                     behind.</p>
                 <hr />
                 <h3>Featured Teachers</h3>
-                <Link to="featured/HTML/Tommy">Tommy Wingo</Link>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Name"/>
+                    <input type="text" placeholder="Topic"/>
+                    <button type="submit">Go!</button>
+                </form>
             </div>
         );
     }
